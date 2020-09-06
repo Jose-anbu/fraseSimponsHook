d07e50245901a4b7e110146177c66cea6e4973a8
José A. Burgos <jose_7892@hotmail.com>
2020-09-06 20:16:21 -0300

    Agregamos un spinner

--- src/App.js
+++ src/App.js
@@ -2,22 +2,34 @@ import React, { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Frase from "./components/Frase";
+import Spinner from "./components/Spinner";
 
 function App() {
   const [fraseSimpsons, setFraseSimpsons] = useState({});
+  const [loader, setLoader] = useState(false);
 
   const consultarAPI = async () => {
+    setLoader(true);
     // fetch: se coloca la dirección de donde voy a traer los datos. Se conecta a un servidor
     const api = await fetch("https://thesimpsonsquoteapi.glitch.me/quotes"); // GET: en una operación GET no hace falta poner el 2do parámetro, pero para subir, modificar o eliminar se elimina un 2do parámetro
     const respuesta = await api.json(); // para acceder al objeto json y guardarla en respuesta
     console.log(api);
     console.log(respuesta[0]);
-    setFraseSimpsons(respuesta[0]);
+    setTimeout(() => { // setTimeout: lo usamos para generar un tiempo que nos permita visualizar el funcionamiento del spinner, solo para verlo en la práctica pero no hay que ponerlo en un proyecto con una consulta a DB real.
+      setFraseSimpsons(respuesta[0]);
+      setLoader(false);
+    }, 2000);
   };
 
   useEffect(() => {
     consultarAPI();
-  },[]);
+  }, []);
+
+  const cargarComponente = loader ? (
+    <Spinner></Spinner>
+  ) : (
+    <Frase fraseSimpsons={fraseSimpsons}></Frase>
+  );
 
   return (
     <section className="container my-5">
@@ -27,11 +39,16 @@ function App() {
           alt="logo de los Simpsons"
           className="w-75 mb-4"
         />
-        <button className="btn btn-warning my-4 w-50 shadow" onClick={()=>{
-          consultarAPI();
-        }}>Obtener frase</button>
+        <button
+          className="btn btn-warning my-4 w-50 shadow"
+          onClick={() => {
+            consultarAPI();
+          }}
+        >
+          Obtener frase
+        </button>
       </article>
-      <Frase fraseSimpsons={fraseSimpsons}></Frase>
+      {cargarComponente}
     </section>
   );
 }
